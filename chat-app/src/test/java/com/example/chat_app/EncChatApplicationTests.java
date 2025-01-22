1229e3ab557ed7b013f3ee4b9c274b9dc38814ec
Mohammed Sinhah <sinhah@crc-dot1x-nat-10-239-132-154.bu.edu>
2025-01-22 18:52:57 -0500

    Finally worked out the workspace

---
+++ chat-app/src/test/java/com/example/chat_app/EncChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
